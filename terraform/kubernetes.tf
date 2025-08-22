e036aa701129fce007c1027baa5d5044a60ca2d9
ualia946 <i946@inlumine.ual.es>
2025-08-22 16:46:52 +0200

    Arreglo de fallos en la configuración de los archivos .tf

--- terraform/kubernetes.tf
+++ terraform/kubernetes.tf
@@ -1,14 +1,15 @@
 resource "azurerm_kubernetes_cluster" "aks" {
-  name                = "aks-terraform-dev-francecentral"
+  name                = "aks-voteapp-dev-frcentral"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "aks-voteapp-dev"
 
   default_node_pool {
     name    = "default"
-    vm_size = "Standard_D2_v2"
+    vm_size = "standard_d2s_v3"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
- 
+  
+    enable_auto_scaling = true
     min_count            = 1
     max_count            = 3
   }
